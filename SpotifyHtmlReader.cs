08692405f19ad5bf0cc26647962db285d50f6482
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2021-04-17 23:15:07 +0300

    User-Agent

--- SpotifyHtmlReader.cs
+++ SpotifyHtmlReader.cs
@@ -5,13 +5,16 @@ using System.Text.RegularExpressions;
 class SpotifyHtmlReader {
 
     public static TrackMetadata Read(string id) {
+        var ua = "curl/7.55.1";
         using(var web = new WebClient()) {
             var result = new TrackMetadata();
 
             var trackUrl = "https://open.spotify.com/track/" + id;
+            web.Headers[HttpRequestHeader.UserAgent] = ua;
             var trackHtml = web.DownloadString(trackUrl);
 
             var albumUrl = ReadMeta(trackHtml, "music:album");
+            web.Headers[HttpRequestHeader.UserAgent] = ua;
             var albumHtml = web.DownloadString(albumUrl);
 
             var coverUrl = ReadMeta(trackHtml, "og:image");
@@ -22,6 +25,7 @@ class SpotifyHtmlReader {
             result.TrackNumber = Convert.ToUInt32(ReadMeta(trackHtml, "music:album:track"));
             result.Year = Convert.ToUInt32(ReadMeta(trackHtml, "music:release_date")?.Substring(0, 4));
             result.SourceUrl = trackUrl;
+            web.Headers[HttpRequestHeader.UserAgent] = ua;
             result.Artwork = web.DownloadData(coverUrl);
 
             return result;
